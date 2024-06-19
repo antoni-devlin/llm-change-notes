5bd433f25e2143f725205bac9130e545f8b0c648
antoni-devlin <antoni.devlin.gmail.com>
2024-06-19 15:13:00 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+import generateRouter from "./routes/generate.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+const port = 3000;
+
+// Middleware
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+// Routes
+app.use("/generate", generateRouter);
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
